bf0d5dcfd0bcabebfb1062e2c7c36e7bd4a97131
KniteenK <143118531+KniteenK@users.noreply.github.com>
2024-01-06 20:44:46 +0530

    Update index.js
    Made the data in a tabular form

--- index.js
+++ index.js
@@ -30,31 +30,47 @@ app.get('/', async (req, res) => {
 
 function generateHTML(votes) {
   return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Problem Solvers</title>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Problem Solvers</title>
       <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
+      </head>
+      <body>
+      <h1> Best-Of-CF </h1> 
+      
       <div id="solverList">
-        ${generateVoteList(votes)}
-      </div>
-    </body>
-    </html>
-  `;
+      <div class="problems-information">
+      <table>
+        <thead>
+        <tr>
+            <th>Index</th>
+            <th>Problem</th>
+            <th>Votes</th>
+          </tr>
+          </thead>
+        <tbody>
+          ${generateVoteList(votes)}
+        </tbody>
+        </table>
+        </div>
+        </div>
+        </body>
+        </html> `;
 }
 
 function generateVoteList(votes) {
-  return votes.map(vote => `
-    <div class="vote-container">
-      <h2><a href="https://codeforces.com/problemset/problem/${vote.problem}" target="_blank">${vote.problem}</a></h2>
-      <p>${vote.names.length}</p>
-    </div>
-  `).join('');
+  return votes.map((vote, index) => `
+            <tr>
+            <td>${index + 1}</td>
+      <td><a href="https://codeforces.com/problemset/problem/${vote.problem}" target="_blank">${vote.problem}</a></td>
+      <td>${vote.names.length}</td>
+      </tr>
+      `).join('');
 }
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
